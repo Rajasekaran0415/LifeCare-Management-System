3b3dcfaaaec084964e900abd3aa0719efcf0e360
Rajasekaran0415 <rajamani344322@gmail.com>
2026-02-26 09:09:32 +0530

    prescription change

--- src/main/java/com/Healthmanagement/HealthManagement/Controller/PrescriptionController.java
+++ src/main/java/com/Healthmanagement/HealthManagement/Controller/PrescriptionController.java
@@ -17,7 +17,7 @@ import com.Healthmanagement.HealthManagement.Services.PrescriptionServices;
 
 @RestController
 @RequestMapping("/prescription")
-@CrossOrigin(origins = "http://127.0.0.1:5500")// allow frontend to call
+//@CrossOrigin(origins = "http://127.0.0.1:5500")// allow frontend to call
 public class PrescriptionController {
 	@Autowired
 	PrescriptionServices prescriptionservices;
