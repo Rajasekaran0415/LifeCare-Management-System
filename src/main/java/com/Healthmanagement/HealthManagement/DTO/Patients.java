5d1b99e7698e6cd5f2f1dcfad109f4e44f062830
Rajasekaran0415 <rajamani344322@gmail.com>
2026-08-29 13:10:29 +0530

    Added Profile

--- src/main/java/com/Healthmanagement/HealthManagement/DTO/Patients.java
+++ src/main/java/com/Healthmanagement/HealthManagement/DTO/Patients.java
@@ -10,10 +10,12 @@ public class Patients {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	private String loginId;
 	private String name;
 	private String password;
 	private String gender;
 	private int age;
+	private int weight;
 	private String bloodgroup;
 
 	public String getGender() {
@@ -22,6 +24,22 @@ public class Patients {
 
 	public void setGender(String gender) {
 		this.gender = gender;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
 	}
 
 	public int getId() {
@@ -64,13 +82,16 @@ public class Patients {
 		this.bloodgroup = bloodgroup;
 	}
 
-	public Patients(int id, String name, String password, String gender, int age, String bloodgroup) {
+	public Patients(int id, String loginId, String name, String password, String gender, int age, int weight,
+			String bloodgroup) {
 		super();
 		this.id = id;
+		this.loginId = loginId;
 		this.name = name;
 		this.password = password;
 		this.gender = gender;
 		this.age = age;
+		this.weight = weight;
 		this.bloodgroup = bloodgroup;
 	}
 
@@ -80,8 +101,8 @@ public class Patients {
 
 	@Override
 	public String toString() {
-		return "Patients [id=" + id + ", name=" + name + ", password=" + password + ", gender=" + gender + ", age="
-				+ age + ", bloodgroup=" + bloodgroup + "]";
+		return "Patients [id=" + id + ", loginId=" + loginId + ", name=" + name + ", password=" + password
+				+ ", gender=" + gender + ", age=" + age + ", weight=" + weight + ", bloodgroup=" + bloodgroup + "]";
 	}
 
 }
